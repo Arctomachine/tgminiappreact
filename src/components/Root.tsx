482d20eb917d48dba06268d3107cffc81b5caf94
Alexander Gerasimov <2632391-Arctomachine@users.noreply.gitlab.com>
2024-08-12 22:58:50 +0300

    react compiler

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,49 +1,44 @@
-import { SDKProvider, useLaunchParams } from '@telegram-apps/sdk-react';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { type FC, useEffect, useMemo } from 'react';
+import { SDKProvider, useLaunchParams } from '@telegram-apps/sdk-react'
+// import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { type FC, useEffect } from 'react'
 
-import { App } from '@/components/App.tsx';
-import { ErrorBoundary } from '@/components/ErrorBoundary.tsx';
+import { App } from '@/components/App.tsx'
+import { ErrorBoundary } from '@/components/ErrorBoundary.tsx'
 
 const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
-  <div>
-    <p>An unhandled error occurred:</p>
-    <blockquote>
-      <code>
-        {error instanceof Error
-          ? error.message
-          : typeof error === 'string'
-            ? error
-            : JSON.stringify(error)}
-      </code>
-    </blockquote>
-  </div>
-);
+	<div>
+		<p>An unhandled error occurred:</p>
+		<blockquote>
+			<code>
+				{error instanceof Error
+					? error.message
+					: typeof error === 'string'
+						? error
+						: JSON.stringify(error)}
+			</code>
+		</blockquote>
+	</div>
+)
 
 const Inner: FC = () => {
-  const debug = useLaunchParams().startParam === 'debug';
-  const manifestUrl = useMemo(() => {
-    return new URL('tonconnect-manifest.json', window.location.href).toString();
-  }, []);
+	const debug = useLaunchParams().startParam === 'debug'
 
-  // Enable debug mode to see all the methods sent and events received.
-  useEffect(() => {
-    if (debug) {
-      import('eruda').then((lib) => lib.default.init());
-    }
-  }, [debug]);
+	// Enable debug mode to see all the methods sent and events received.
+	useEffect(() => {
+		if (debug) {
+			import('eruda').then((lib) => lib.default.init())
+		}
+	}, [debug])
 
-  return (
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <SDKProvider acceptCustomStyles debug={debug}>
-        <App/>
-      </SDKProvider>
-    </TonConnectUIProvider>
-  );
-};
+	return (
+		<SDKProvider acceptCustomStyles debug={debug}>
+			<App />
+		</SDKProvider>
+	)
+}
 
 export const Root: FC = () => (
-  <ErrorBoundary fallback={ErrorBoundaryError}>
-    <Inner/>
-  </ErrorBoundary>
-);
+	<ErrorBoundary fallback={ErrorBoundaryError}>
+		<Inner />
+	</ErrorBoundary>
+)
