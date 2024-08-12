4acbfe3e426fa300a04f69b1c4cdf9b9a8b12678
Alexander Gerasimov <2632391-Arctomachine@users.noreply.gitlab.com>
2024-08-13 00:27:37 +0300

    небольшой рефакторинг

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,9 +1,6 @@
 import type { ComponentType, JSX } from 'react'
 
 import { IndexPage } from '@/pages/IndexPage/IndexPage'
-import { InitDataPage } from '@/pages/InitDataPage/InitDataPage'
-import { LaunchParamsPage } from '@/pages/LaunchParamsPage/LaunchParamsPage.tsx'
-import { ThemeParamsPage } from '@/pages/ThemeParamsPage/ThemeParamsPage.tsx'
 
 interface Route {
 	path: string
@@ -12,13 +9,4 @@ interface Route {
 	icon?: JSX.Element
 }
 
-export const routes: Route[] = [
-	{ path: '/', Component: IndexPage },
-	{ path: '/init-data', Component: InitDataPage, title: 'Init Data' },
-	{ path: '/theme-params', Component: ThemeParamsPage, title: 'Theme Params' },
-	{
-		path: '/launch-params',
-		Component: LaunchParamsPage,
-		title: 'Launch Params',
-	},
-]
+export const routes: Route[] = [{ path: '/', Component: IndexPage }]
